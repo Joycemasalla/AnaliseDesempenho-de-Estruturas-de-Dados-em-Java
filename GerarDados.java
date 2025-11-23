d6d72517929088d497b1258df824310b3dae7b8b
Joyce <joycemasallajorge2006@gmail.com>
2025-11-23 10:46:10 -0300

    altera nome DadosTeste para GerarDados

---
+++ GerarDados.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class GerarDados {
+
+    // Ordenados
+    public int[] gerarDadosOrdenados(int quantidadeElementos) {
+        int[] dados = new int[quantidadeElementos];
+        for (int i = 0; i < quantidadeElementos; i++) {
+            dados[i] = i;
+        }
+        return dados;
+    }
+
+    // Inversamente Ordenados
+    public int[] gerarDadosInversos(int quantidadeElementos) {
+        int[] dados = new int[quantidadeElementos];
+        for (int i = 0; i < quantidadeElementos; i++) {
+            dados[i] = quantidadeElementos - i;
+        }
+        return dados;
+    }
+
+    // Aleatórios
+    public int[] gerarDadosAleatorios(int quantidadeElementos) {
+        int[] dados = new int[quantidadeElementos];
+        Random random = new Random();
+        for (int i = 0; i < quantidadeElementos; i++) {
+            dados[i] = random.nextInt(quantidadeElementos);
+        }
+        return dados;
+    }
+}
